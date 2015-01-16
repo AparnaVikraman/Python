6ff5b08538ba4aea7956d3e6ec509e51ff823c5f
Aparna <aparna.v@hp.com>
2015-01-16 16:50:36 +0530

    python commit

---
+++ for.py
@@ -0,0 +1,8 @@
+import sys
+i={1,2,3}
+print("series 1")
+for x in i:
+	print(x)
+print("series 2")
+for x in range(5):
+	print(x)
---
+++ if.py
@@ -0,0 +1,7 @@
+import sys
+x=2
+print("if condition")
+if x<3:
+	print(x)
+else:
+	print("no")
